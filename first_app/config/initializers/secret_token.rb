8cadae8abee811cd516f1b35aba1786b8574cb02
hitsumabushi <053023.math@gmail.com>
2013-12-16 01:03:32 +0900

    Rewriting root dir

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '8d3706361af9a3bac539f3ae8b9bf0fd4f9ad25f974a73196173c991ef3617a55684ae2a572577c52e0bbea99c8bed1d1eb1676bd881a795815c878eba0aa6fe'
---
+++ vendor/bundle/ruby/2.0.0/gems/sqlite3-1.3.8/ext/sqlite3/siteconf20131026-2036-hgck6e.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131026-2036-g8xs23"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
